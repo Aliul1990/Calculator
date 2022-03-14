c5b013848abf7b5bec6822d4af6e6886efae9658
MD. ALIUL ISLAM <36182749+Aliul1990@users.noreply.github.com>
2022-03-14 20:22:19 +0600

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,9 @@
+    function calcNumbers(result){
+        form.displayResult.value=form.displayResult.value+result;
+    }
+    function Clear(result) {
+        form.displayResult.value="";
+    }
+    function AcClear(result){
+        form.displayResult.value="";
+    }
